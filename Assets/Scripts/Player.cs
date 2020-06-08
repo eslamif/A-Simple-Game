3114a03259321bf11a70a050080fec8f3dd42191
Frank Eslami <eslamifrank@gmail.com>
2020-06-08 16:36:06 -0700

    Player movement input works

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField] private float movementSpeed = 5f;
+
+    private Rigidbody rb;
+    private Vector3 inputVector;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        inputVector = new Vector3(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y, Input.GetAxis("Vertical") * movementSpeed);
+        transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = inputVector;
+    }
+}
