a51b8ff362d2f515ce0c449523ff19078658a34b
Frank Eslami <eslamifrank@gmail.com>
2020-06-08 16:50:59 -0700

    isGrounded() works

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float movementSpeed = 10f;
+    [SerializeField] private float jumpSpeed = 20f;
 
     private Rigidbody rb;
     private Vector3 inputVector;
+    private bool jump = false;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
@@ -15,9 +17,24 @@ public class Player : MonoBehaviour {
     void Update() {
         inputVector = new Vector3(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y, Input.GetAxis("Vertical") * movementSpeed);
         transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
     }
 
     private void FixedUpdate() {
         rb.velocity = inputVector;
+
+        if (jump && isGrounded()) {
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            jump = false;
+        }
+    }
+
+    private bool isGrounded() {
+        float distance = GetComponent<Collider>().bounds.extents.y + 0.01f;
+        Ray ray = new Ray(transform.position, Vector3.down);
+        return Physics.Raycast(ray, distance);
     }
 }
