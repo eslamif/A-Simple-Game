8776615b5b517fc5ff3617453fc4409a9c47f61e
Frank Eslami <eslamifrank@gmail.com>
2020-06-08 20:18:32 -0700

    Game manager created

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour {
+    [SerializeField] private int currentScene;
+    [SerializeField] private bool lastScene;
+
+    private int nextScene;
+
+    void Start() {
+        nextScene = currentScene + 1;
+    }
+
+    public void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ReloadCurrentScene() {
+        LoadScene("Level-" + currentScene);
+    }
+
+    public void LoadNextScene() {
+        if (!lastScene) {
+
+            string sceneName = "Level-" + nextScene;
+            LoadScene(sceneName);
+        }
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
