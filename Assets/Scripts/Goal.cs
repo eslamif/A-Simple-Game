e4c5897446bcb040f1a28028f7e60512d6e42174
Frank Eslami <eslamifrank@gmail.com>
2020-06-08 21:24:08 -0700

    Goal script created

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+    private int requiredCoins;
+    private Game game;
+
+    void Start() {
+        requiredCoins = GameObject.FindGameObjectsWithTag("Coin").Length;
+        game = GameObject.FindObjectOfType<Game>();
+    }
+
+    public void CheckForCompletion(int coinsCollected) {
+        if (coinsCollected >= requiredCoins) {
+            game.LoadNextScene();
+        }
+        else {
+            Debug.Log("You need more coins!");
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -66,6 +66,7 @@ public class Player : MonoBehaviour {
                 break;
 
             case "Goal":
+                other.GetComponent<Goal>().CheckForCompletion(coinsCollected);
                 break;
 
             default:
