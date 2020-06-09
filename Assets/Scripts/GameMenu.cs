1d1adedb72b92a7c79ea5af1f20bca2b758c80cb
Frank Eslami <eslamifrank@gmail.com>
2020-06-09 15:24:03 -0700

    Menu added

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour {
+    private GameObject menuPanel;
+
+    private void Start() {
+        menuPanel = transform.GetChild(0).gameObject;
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("GameMenu")) {
+            menuPanel.SetActive(!menuPanel.activeSelf);
+        }
+    }
+}
